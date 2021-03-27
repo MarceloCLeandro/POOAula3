c72be09931b011ba73e0f52185d6184a11c90241
MarceloCLeandro <65774706+MarceloCLeandro@users.noreply.github.com>
2021-03-26 21:00:40 -0300

    Alterando arquivos
    Troca/substituição

---
+++ src/main/java/com/mycompany/exercicio_entregar/Avaliacao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercicio_entregar;
+
+/**
+ *
+ * @author mcl_l
+ */
+class Avaliacao {
+    
+    public static void main(String[] args) {
+        Percentual objPercentual1 = new Percentual();
+        System.out.println("" +objPercentual1.toString());
+        
+        Percentual objPercentual2 = new Percentual();
+        System.out.println("" +objPercentual2.toString());
+        
+        Percentual objPercentual3 = new Percentual();
+        System.out.println("" +objPercentual3.toString());
+        
+    }
+}
